22edb45a4f52d76bab7b556c44e28f78bf20de2c
snealbli <dev@nealblim.com>
2026-03-27 10:39:14 -0400

    Updated 'compose.hpp': cosmetic changes.

--- djinterp/inc/core/functional/compose.hpp
+++ djinterp/inc/core/functional/compose.hpp
@@ -1,5 +1,5 @@
 /******************************************************************************
-* djinterp [functional]                                          compose.hpp
+* djinterp [functional]                                            compose.hpp
 *
 * Template function composition and partial application (C++).
 *   Provides SFINAE-constrained composed transformers, partial application
@@ -14,13 +14,13 @@
 *     - memoization wrapper
 *     - tap (side-effect injection)
 *
-* path:      \inc\functional\compose.hpp
+* path:      /inc/functional/compose.hpp
 * link(s):   TBA
 * author(s): Samuel 'teer' Neal-Blim                          date: 2026.02.19
 ******************************************************************************/
 
-#ifndef DJINTERP_FUNCTIONAL_COMPOSE_HPP_
-#define DJINTERP_FUNCTIONAL_COMPOSE_HPP_ 1
+#ifndef DJINTERP_FUNCTIONAL_COMPOSE_
+#define DJINTERP_FUNCTIONAL_COMPOSE_ 1
 
 #include <cstddef>
 #include <functional>
@@ -52,10 +52,6 @@ NS_INTERNAL
              typename _Second>
     class composed_transformer_helper
     {
-    private:
-        _First  m_first;   // applied first (g in f(g(x)))
-        _Second m_second;  // applied second (f in f(g(x)))
-
     public:
         template<typename _F1Fwd,
                  typename _F2Fwd>
@@ -90,53 +86,54 @@ NS_INTERNAL
         // accessors for introspection
         D_CONSTEXPR const _First&  first()  const { return m_first; }
         D_CONSTEXPR const _Second& second() const { return m_second; }
+
+    private:
+        _First  m_first;   // applied first (g in f(g(x)))
+        _Second m_second;  // applied second (f in f(g(x)))
     };
 
     // partial_consumer_helper
     //   helper: partially applied consumer with bound context.
     // Mirrors d_partial_consumer from compose.h.
-    template<typename _Fn,
+    template<typename _Function,
              typename _BoundArg>
     class partial_consumer_helper
     {
-    private:
-        _Fn       m_fn;
-        _BoundArg m_bound;
-
     public:
-        template<typename _FnFwd,
+        template<typename _FunctionFwd,
                  typename _ArgFwd>
         D_CONSTEXPR
-        partial_consumer_helper(_FnFwd&& _fn, _ArgFwd&& _arg)
-            : m_fn(std::forward<_FnFwd>(_fn))
+        partial_consumer_helper(_FunctionFwd&& _function, _ArgFwd&& _arg)
+            : m_fn(std::forward<_FunctionFwd>(_function))
             , m_bound(std::forward<_ArgFwd>(_arg))
         {}
 
         template<typename... _Args>
         D_CONSTEXPR
         auto operator()(_Args&&... _args) const
-            -> decltype(std::declval<const _Fn&>()(
+            -> decltype(std::declval<const _Function&>()(
                 std::forward<_Args>(_args)...,
                 std::declval<const _BoundArg&>()))
         {
             return m_fn(std::forward<_Args>(_args)..., m_bound);
         }
+
+    private:
+        _Function m_fn;
+        _BoundArg m_bound;
     };
 
     // tap_helper
     //   helper: passes value through a side-effect function, returning
     // the original value unchanged.
-    template<typename _Fn>
+    template<typename _Function>
     class tap_helper
     {
-    private:
-        _Fn m_fn;
-
     public:
-        template<typename _FnFwd>
+        template<typename _FunctionFwd>
         explicit D_CONSTEXPR
-        tap_helper(_FnFwd&& _fn)
-            : m_fn(std::forward<_FnFwd>(_fn))
+        tap_helper(_FunctionFwd&& _function)
+            : m_fn(std::forward<_FunctionFwd>(_function))
         {}
 
         template<typename _Type>
@@ -147,23 +144,22 @@ NS_INTERNAL
 
             return _value;
         }
+
+    private:
+        _Function m_fn;
     };
 
     // memoize_helper
     //   helper: caches results of a pure function.
-    template<typename _Fn,
+    template<typename _Function,
              typename _Input,
              typename _Output>
     class memoize_helper
     {
-    private:
-        _Fn                                 m_fn;
-        mutable std::map<_Input, _Output>   m_cache;
-
     public:
-        template<typename _FnFwd>
-        explicit memoize_helper(_FnFwd&& _fn)
-            : m_fn(std::forward<_FnFwd>(_fn))
+        template<typename _FunctionFwd>
+        explicit memoize_helper(_FunctionFwd&& _function)
+            : m_fn(std::forward<_FunctionFwd>(_function))
         {}
 
         _Output operator()(const _Input& _input) const
@@ -193,6 +189,10 @@ NS_INTERNAL
         {
             return m_cache.size();
         }
+
+    private:
+        _Function                           m_fn;
+        mutable std::map<_Input, _Output>   m_cache;
     };
 
 NS_END  // internal
@@ -210,8 +210,12 @@ template<typename _First,
          typename _Second>
 D_CONSTEXPR
 internal::composed_transformer_helper<typename std::decay<_First>::type,
-                                    typename std::decay<_Second>::type>
-compose_transformer(_First&& _first, _Second&& _second)
+                                      typename std::decay<_Second>::type>
+compose_transformer
+(
+    _First&&  _first, 
+    _Second&& _second
+)
 {
     return internal::composed_transformer_helper<
         typename std::decay<_First>::type,
@@ -228,51 +232,61 @@ compose_transformer(_First&& _first, _Second&& _second)
 // compose_all (right-to-left)
 //   function: composes N functions right-to-left.
 // compose_all(f, g, h)(x) = f(g(h(x)))
-template<typename _Fn>
+template<typename _Function>
 D_CONSTEXPR
-auto compose_all(_Fn&& _fn)
-    -> typename std::decay<_Fn>::type
+auto compose_all(
+    _Function&& _function
+) -> typename std::decay<_Function>::type
 {
-    return std::forward<_Fn>(_fn);
+    return std::forward<_Function>(_function);
 }
 
-template<typename _Fn1,
-         typename _Fn2,
-         typename... _Fns>
+template<typename    _Function1,
+         typename    _Function2,
+         typename... _Functions>
 D_CONSTEXPR
-auto compose_all(_Fn1&& _fn1, _Fn2&& _fn2, _Fns&&... _rest)
-    -> decltype(compose(std::forward<_Fn1>(_fn1),
-                        compose_all(std::forward<_Fn2>(_fn2),
-                                    std::forward<_Fns>(_rest)...)))
+auto compose_all(
+    _Function1&&    _function1,
+    _Function2&&    _function2,
+    _Functions&&... _rest
+)
+-> decltype(compose(std::forward<_Function1>(_function1),
+                    compose_all(std::forward<_Function2>(_function2),
+                                std::forward<_Functions>(_rest)...)))
 {
-    return compose(std::forward<_Fn1>(_fn1),
-                   compose_all(std::forward<_Fn2>(_fn2),
-                               std::forward<_Fns>(_rest)...));
+    return compose(std::forward<_Function1>(_function1),
+                   compose_all(std::forward<_Function2>(_function2),
+                               std::forward<_Functions>(_rest)...));
 }
 
 // pipe_all (left-to-right)
 //   function: composes N functions left-to-right.
 // pipe_all(f, g, h)(x) = h(g(f(x)))
-template<typename _Fn>
+template<typename _Function>
 D_CONSTEXPR
-auto pipe_all(_Fn&& _fn)
-    -> typename std::decay<_Fn>::type
+auto pipe_all(
+    _Function&& _function
+) -> typename std::decay<_Function>::type
 {
-    return std::forward<_Fn>(_fn);
+    return std::forward<_Function>(_function);
 }
 
-template<typename _Fn1,
-         typename _Fn2,
-         typename... _Fns>
+template<typename    _Function1,
+         typename    _Function2,
+         typename... _Functions>
 D_CONSTEXPR
-auto pipe_all(_Fn1&& _fn1, _Fn2&& _fn2, _Fns&&... _rest)
-    -> decltype(pipe_all(pipe(std::forward<_Fn1>(_fn1),
-                              std::forward<_Fn2>(_fn2)),
-                         std::forward<_Fns>(_rest)...))
+auto pipe_all
+(
+    _Function1&&    _function1,
+    _Function2&&    _function2,
+    _Functions&&... _rest
+) -> decltype(pipe_all(pipe(std::forward<_Function1>(_function1),
+                            std::forward<_Function2>(_function2)),
+                            std::forward<_Functions>(_rest)...))
 {
-    return pipe_all(pipe(std::forward<_Fn1>(_fn1),
-                         std::forward<_Fn2>(_fn2)),
-                    std::forward<_Fns>(_rest)...);
+    return pipe_all(pipe(std::forward<_Function1>(_function1),
+                         std::forward<_Function2>(_function2)),
+                    std::forward<_Functions>(_rest)...);
 }
 
 
@@ -283,18 +297,23 @@ auto pipe_all(_Fn1&& _fn1, _Fn2&& _fn2, _Fns&&... _rest)
 // partial_back
 //   function: partially applies the last argument of a function.
 // partial_back(f, z)(x, y) = f(x, y, z)
-template<typename _Fn,
+template<typename _Function,
          typename _Arg>
 D_CONSTEXPR
-internal::partial_consumer_helper<typename std::decay<_Fn>::type,
-                                typename std::decay<_Arg>::type>
-partial_back(_Fn&& _fn, _Arg&& _arg)
+internal::partial_consumer_helper<typename std::decay<_Function>::type,
+                                  typename std::decay<_Arg>::type>
+partial_back
+(
+    _Function&& _function,
+    _Arg&&      _arg
+)
 {
     return internal::partial_consumer_helper<
-        typename std::decay<_Fn>::type,
+        typename std::decay<_Function>::type,
         typename std::decay<_Arg>::type>(
-            std::forward<_Fn>(_fn),
-            std::forward<_Arg>(_arg));
+            std::forward<_Function>(_function),
+            std::forward<_Arg>(_arg)
+        );
 }
 
 // Note: partial_front (bind_front) is already provided in functional.hpp
@@ -309,13 +328,13 @@ partial_back(_Fn&& _fn, _Arg&& _arg)
 //   function: creates a pass-through function that executes a side-effect.
 // tap(f)(x) calls f(x) then returns x unchanged.
 // Useful for debugging in composition chains.
-template<typename _Fn>
+template<typename _Function>
 D_CONSTEXPR
-internal::tap_helper<typename std::decay<_Fn>::type>
-tap(_Fn&& _fn)
+internal::tap_helper<typename std::decay<_Function>::type>
+tap(_Function&& _function)
 {
-    return internal::tap_helper<typename std::decay<_Fn>::type>(
-        std::forward<_Fn>(_fn));
+    return internal::tap_helper<typename std::decay<_Function>::type>(
+        std::forward<_Function>(_function));
 }
 
 
@@ -327,16 +346,19 @@ tap(_Fn&& _fn)
 //   function: wraps a pure function with a cache.
 // The function must be deterministic (same input -> same output).
 // The input type must be comparable (for use as map key).
-template<typename _Fn,
+template<typename _Function,
          typename _Input,
-         typename _Output = callable_result_t<_Fn, const _Input&>>
-internal::memoize_helper<typename std::decay<_Fn>::type, _Input, _Output>
-memoize(_Fn&& _fn)
+         typename _Output = callable_result_t<_Function, const _Input&>>
+internal::memoize_helper<typename std::decay<_Function>::type, _Input, _Output>
+memoize
+(
+    _Function&& _function
+)
 {
     return internal::memoize_helper<
-        typename std::decay<_Fn>::type,
-        _Input, _Output>(
-            std::forward<_Fn>(_fn));
+        typename std::decay<_Function>::type, _Input, _Output>(
+            std::forward<_Function>(_function)
+        );
 }
 
 
@@ -348,28 +370,28 @@ NS_INTERNAL
 
     // fix_helper
     //   helper: Y combinator for expressing recursive lambdas.
-    template<typename _Fn>
+    template<typename _Function>
     class fix_helper
     {
-    private:
-        _Fn m_fn;
-
     public:
-        template<typename _FnFwd>
+        template<typename _FunctionFwd>
         explicit D_CONSTEXPR
-        fix_helper(_FnFwd&& _fn)
-            : m_fn(std::forward<_FnFwd>(_fn))
+        fix_helper(_FunctionFwd&& _function)
+            : m_fn(std::forward<_FunctionFwd>(_function))
         {}
 
         template<typename... _Args>
         D_CONSTEXPR
         auto operator()(_Args&&... _args) const
-            -> decltype(std::declval<const _Fn&>()(
+            -> decltype(std::declval<const _Function&>()(
                 std::declval<const fix_helper&>(),
                 std::forward<_Args>(_args)...))
         {
             return m_fn(*this, std::forward<_Args>(_args)...);
         }
+
+    private:
+        _Function m_fn;
     };
 
 NS_END  // internal
@@ -381,13 +403,13 @@ NS_END  // internal
 //       return n <= 1 ? 1 : n * self(n - 1);
 //   });
 //   factorial(5);  // 120
-template<typename _Fn>
+template<typename _Function>
 D_CONSTEXPR
-internal::fix_helper<typename std::decay<_Fn>::type>
-fix(_Fn&& _fn)
+internal::fix_helper<typename std::decay<_Function>::type>
+fix(_Function&& _function)
 {
-    return internal::fix_helper<typename std::decay<_Fn>::type>(
-        std::forward<_Fn>(_fn));
+    return internal::fix_helper<typename std::decay<_Function>::type>(
+        std::forward<_Function>(_function));
 }
 
 
